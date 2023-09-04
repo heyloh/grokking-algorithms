ba7011dcf297bb331eaf7291c73b83cc8e8bf2b2
Lohana Torres <lt.leopoldo@gmail.com>
2023-09-03 22:13:21 -0300

    test: valida total de etapas

--- src/01-introduction/binary-search.spec.ts
+++ src/01-introduction/binary-search.spec.ts
@@ -2,18 +2,32 @@ import { binarySearch } from "./binary-search";
 
 describe("Binary Search", () => {
   it("should return 2 when given 5", () => {
-    expect(binarySearch([1,3,5,7,9], 5)).toBe(2)
+    const { position } = binarySearch([1,3,5,7,9], 5)
+    expect(position).toBe(2)
   });
 
   it("should return 1 when given 3", () => {
-    expect(binarySearch([1,3,5,7,9], 3)).toBe(1)
+    const { position } = binarySearch([1,3,5,7,9], 3)
+    expect(position).toBe(1)
   });
 
   it("should return 3 when given 7", () => {
-    expect(binarySearch([1,3,5,7,9], 7)).toBe(3)
+    const { position } = binarySearch([1,3,5,7,9], 7)
+    expect(position).toBe(3)
   });
 
   it("should return null when given -1", () => {
-    expect(binarySearch([1,3,5,7,9], -1)).toBe(null)
+    const { position } = binarySearch([1,3,5,7,9], -1)
+    expect(position).toBe(null)
   });
+
+  it("should take 8 steps to search a list with 128 items", () => {
+    const { steps } = binarySearch([...Array(128).keys()], 127)
+    expect(steps).toBe(8)
+  })
+
+  it("should take 9 steps to search a list with 256 items", () => {
+    const { steps } = binarySearch([...Array(256).keys()], 255)
+    expect(steps).toBe(9)
+  })
 });
